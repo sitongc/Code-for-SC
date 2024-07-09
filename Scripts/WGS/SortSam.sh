d47a583c92e05eab3c2c976ba803f90aab151391
sitongc <48114157+sitongc@users.noreply.github.com>
2024-07-09 19:24:22 -0400

    Delete Scripts/WGS/SortSam.sh

--- Scripts/WGS/SortSam.sh
+++
@@ -1,9 +0,0 @@
-# Sort sam files
-echo "Sort sam file"
-gatk SortSam \
---INPUT /groups/ligrp/sitongchen/scrna/sam/${filename}.sam \
---OUTPUT /groups/ligrp/sitongchen/scrna/sortbam/${filename}_sorted.bam \
---SORT_ORDER coordinate
-if [ /groups/ligrp/sitongchen/scrna/sortbam/${filename}_sorted.bam -s ]; then
-rm -f /groups/ligrp/sitongchen/scrna/sam/${filename}.sam
-fi
