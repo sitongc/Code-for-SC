befc099e72aaba2216029bd773c3fec26258e250
sitongc <48114157+sitongc@users.noreply.github.com>
2024-11-17 21:46:51 -0800

    Update scanpy.py

--- Scripts/Spatial/scanpy.py
+++ Scripts/Spatial/scanpy.py
@@ -4,3 +4,7 @@ import scanpy as sc
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+sc.logging.print_versions()
+sc.set_figure_params(facecolor="white", figsize=(8, 8))
+sc.settings.verbosity = 3
